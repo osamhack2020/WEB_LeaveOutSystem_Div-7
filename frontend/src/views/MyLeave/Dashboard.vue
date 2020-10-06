ed0795a6992b6cc6bd3641fbabbc3ac1cebb1a58
Tekiter <tekiter8@gmail.com>
2020-10-06 14:51:20 +0000

    Create dashboard card base

--- frontend/src/views/MyLeave/Dashboard.vue
+++ frontend/src/views/MyLeave/Dashboard.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <CurrentLocation :location="location"></CurrentLocation>
+
+    <v-row>
+      <v-col cols="3"><DashboardCard>asdf</DashboardCard></v-col>
+      <v-col cols="3"><DashboardCard>asdf</DashboardCard></v-col>
+      <v-col cols="3"><DashboardCard>asdf</DashboardCard></v-col>
+      <v-col cols="3"><DashboardCard>asdf</DashboardCard></v-col>
+    </v-row>
   </div>
 </template>
 <script>
 import CurrentLocation from '../../components/myleave/CurrentLocation.vue'
+import DashboardCard from '../../components/myleave/DashboardCard.vue'
 
 export default {
   components: {
-    CurrentLocation
+    CurrentLocation,
+    DashboardCard
   },
   data: () => ({}),
   computed: {
