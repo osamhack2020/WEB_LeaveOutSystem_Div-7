8906728769563ba2b0ab6132726c3b4265a80b71
Tekiter <tekiter8@gmail.com>
2020-10-08 11:23:59 +0000

    Create layout for leave avaliable page

--- frontend/src/views/MyLeave/LeaveAvailable.vue
+++ frontend/src/views/MyLeave/LeaveAvailable.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
     <CurrentLocation :location="location"></CurrentLocation>
+    <v-row>
+      <v-col cols="5">
+        <v-card>
+          <v-list>
+            <v-subheader>출타 종류</v-subheader>
+            <v-list-item-group>
+              <v-list-item>휴가</v-list-item>
+              <v-list-item>외출</v-list-item>
+              <v-list-item>외박</v-list-item>
+            </v-list-item-group>
+            <v-divider></v-divider>
+            <v-btn-toggle multiple tile group>
+              <v-btn>
+                <v-icon>정기</v-icon>
+              </v-btn>
+              <v-btn>
+                <v-icon>포상</v-icon>
+              </v-btn>
+              <v-btn>
+                <v-icon>위로</v-icon>
+              </v-btn>
+              <v-btn>
+                <v-icon>기타</v-icon>
+              </v-btn>
+            </v-btn-toggle>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col cols="7"> </v-col>
+    </v-row>
   </div>
 </template>
 <script>
