b7a14db318e8653369e33e38419c5df42cd5fadd
Tekiter <tekiter8@gmail.com>
2020-10-09 10:48:22 +0000

    Add leave available filter system

--- frontend/src/views/MyLeave/LeaveAvailable.vue
+++ frontend/src/views/MyLeave/LeaveAvailable.vue
@@ -4,40 +4,45 @@
     <v-row>
       <v-col cols="4">
         <v-card>
-          <v-tabs>
+          <v-tabs v-model="currentType">
             <v-tab>휴가</v-tab>
             <v-tab>외출</v-tab>
             <v-tab>외박</v-tab>
           </v-tabs>
           <KindFilter v-model="kindFilterOptions" />
         </v-card>
-        <v-tabs-items>
-          <v-tab-item>
-            <template v-if="availableLoading">
-              <v-skeleton-loader
-                v-for="i in 4"
-                class="mb-3"
-                type="article, actions"
-                :key="`loader-${i}`"
-              ></v-skeleton-loader>
-            </template>
-            <template v-else>
-              <v-card
-                v-for="(item, idx) of currentAvailables"
-                :key="idx"
-                class="mt-3"
-              >
-                <v-card-title> {{ item.kind }} {{ item.type }} </v-card-title>
-                <v-card-text>
-                  <div v-if="item.type === '휴가'">{{ item.amount }}일</div>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn text>사용하기</v-btn>
-                </v-card-actions>
-              </v-card>
-            </template>
+        <template v-if="availableLoading">
+          <v-skeleton-loader
+            v-for="i in 4"
+            class="mb-3"
+            type="article, actions"
+            :key="`loader-${i}`"
+          ></v-skeleton-loader>
+        </template>
+        <v-tabs-items v-else v-model="currentType">
+          <v-tab-item
+            v-for="typ of availableTypes"
+            :key="`leave-tab-${typ.value}`"
+            class="pa-2"
+          >
+            <v-card
+              outlined
+              v-for="(item, idx) of currentAvailables[typ.value]"
+              :key="idx"
+              class="mb-2"
+            >
+              <v-card-title> {{ item.kind }} {{ item.type }} </v-card-title>
+              <v-card-text>
+                <div v-if="item.type === '휴가'">{{ item.amount }}일</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn text>사용하기</v-btn>
+              </v-card-actions>
+            </v-card>
           </v-tab-item>
+          <!-- <v-tab-item>asdf</v-tab-item>
+          <v-tab-item>asdf</v-tab-item> -->
         </v-tabs-items>
       </v-col>
       <v-col cols="4"> </v-col>
@@ -57,16 +62,35 @@ export default {
   data: () => ({
     availables: [],
     availableLoading: false,
-    kindFilterOptions: []
+    kindFilterOptions: [],
+
+    currentType: 0
   }),
   computed: {
     location: () => [
       { text: '대시보드', path: '/myleave' },
       { text: '사용 가능 출타', path: '/myleave/leave-available' }
     ],
+    availableTypes: () => [
+      { value: '휴가' },
+      { value: '외출' },
+      { value: '외박' }
+    ],
 
     currentAvailables() {
-      return this.availables
+      const ret = {}
+
+      for (const item of this.availables) {
+        ret[item.type] = ret[item.type] || []
+        if (this.kindFilterOptions.includes(item.kind)) {
+          ret[item.type].push(item)
+        }
+      }
+
+      return ret
+    },
+    currentVacations() {
+      return this.availables.filter(ava => ava.type === '휴가')
     }
   },
   methods: {
