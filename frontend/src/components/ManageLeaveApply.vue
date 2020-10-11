1c952799f80953b23ebe344489e29d5ac4a41fd1
Tekiter <tekiter8@gmail.com>
2020-10-11 03:03:59 +0000

    Add getAllApplies API

--- frontend/src/components/ManageLeaveApply.vue
+++ frontend/src/components/ManageLeaveApply.vue
@@ -1,8 +1,31 @@
 <template>
-  <div></div>
+  <v-row>
+    <v-col cols="3">
+      <v-card>C1</v-card>
+    </v-col>
+    <v-col cols="9">C2</v-col>
+  </v-row>
 </template>
 <script>
+import leaveAPI from '../services/leave'
+
 export default {
-  data: () => ({})
+  data: () => ({
+    rawApplies: []
+  }),
+  computed: {
+    applies() {
+      return this.rawApplies
+    }
+  },
+  methods: {
+    async loadApplies() {
+      const res = await leaveAPI.adminGetApplies()
+      this.rawApplies = res.data
+    }
+  },
+  async created() {
+    await this.loadApplies()
+  }
 }
 </script>
