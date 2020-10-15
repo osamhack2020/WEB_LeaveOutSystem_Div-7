e2943d25c6a5b697dbb7be2eafbb45ce942bc2d2
Tekiter <tekiter8@gmail.com>
2020-10-15 14:54:18 +0000

    Change dashboard widget design

--- frontend/src/components/myleave/DashboardCard.vue
+++ frontend/src/components/myleave/DashboardCard.vue
@@ -1,10 +1,22 @@
 <template>
   <v-card>
-    <slot></slot>
+    <div v-if="loading" class="d-flex flex-row justify-center">
+      <v-progress-circular
+        :width="3"
+        color="primary"
+        indeterminate
+      ></v-progress-circular>
+    </div>
+    <slot v-else></slot>
   </v-card>
 </template>
 <script>
 export default {
-  props: {}
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
