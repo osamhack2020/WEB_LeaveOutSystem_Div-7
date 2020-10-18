3464780bc1e3bbb41e9c5b3c9b75a4f94388ecb3
ask-to-answer <bkdowhite@gmail.com>
2020-10-18 14:58:25 +0900

    Added footer

---
+++ frontend/src/components/core/FooterBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer dark padless>
+    <v-container class="ma-0 pa-0 container--fluid padless">
+      <v-card
+        flat
+        tile
+        dark
+        class="primary lighten-1 white--text text-center fill-height fluid padless"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>OSAM hackathon</strong>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram']
+  })
+}
+</script>
--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -6,6 +6,5 @@
     >
       현재 소속 부대가 없습니다. 부대 관리자에게 문의하여 소속을 변경하세요.
     </v-alert>
-    <div>main page</div>
   </div>
 </template>
